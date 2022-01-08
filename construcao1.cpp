c38ba9cee77005646fd8abfab894c4b674648fec
Leandro Lucas <leoViolinist22@gmail.com>
2022-01-08 17:43:39 -0300

    ajeitando inf

--- construcao1.cpp
+++ construcao1.cpp
@@ -31,24 +31,38 @@ typedef struct
 
 
 
-
-void Escolher3NumerosAleatorios(tSolucao& s1)
+/****
+ * Escolher3NumerosAleatorios(): Função escolhe 3 números aleatórios para serem adicionados no tour
+ * 
+ * 
+ * 
+ * Parâmetros:
+ *                  s1(entrada e saída): local que será armazenado os números aleatórios
+ *                  quantidade(entrada): limite dos números aleatórios, que é a dimensão da matriz
+ * 
+ * 
+ * Retorno:
+ *                  nenhum
+ * 
+ ****/
+void Escolher3NumerosAleatorios(tSolucao& s1, int quantidade)
 {
     int aleatorio;  /*  Número aleatório determinado    */
     int presente;   /*  Variável que diz, se o número aleatório já foi escolhido ou não */
     //V = {1,2,3,4,5,6,7,8,9, 10}   Vértices totais
-    unsigned seed = time(0);
+
+
+    unsigned seed = time(0);    /*  Criação de uma semente para gerar aleatoriedade */
     srand(seed);
 
-    /*  Gerando o valor aleatorio, e colocando-o logo depois do seu antecessor a partir do inicio   */
-    /*  Inicialmente, gera-se um número aleatório entre 1 e 10, após isso verifica se ele já se encontra no primeiro tour   */
+    /*  Inicialmente, gera-se um número aleatório entre a quantidade relativa a dimensão, após isso verifica se ele já se encontra no primeiro tour   */
     /*  Caso sim, mudamos o valor de presente para 1 e voltamos uma casa no loop, para que seja colocado de fato 3 valores aleatórios   */
     for(int i = 1; i <= 3; i++)
     {
         presente = 0;
 
         /*  Determinação do número aleatório    */
-        aleatorio = (rand() % 10) + 1;
+        aleatorio = (rand() % quantidade) + 1;
         
         /*  Verifica se ele se encontra ou não no tour inicial  */
         for(int i = 0; i < s1.sequencia.size(); i++)
@@ -71,34 +85,34 @@ void Escolher3NumerosAleatorios(tSolucao& s1)
             i--;
         }
 
-        
-        
-        
-
 
     }
 
 }
-void ExibirSolucao(tSolucao *s)
-{
-    for(int i = 0; i < s->sequencia.size(); i++)
-    {
-        cout << s->sequencia[i] << "-> ";
-        
-    }
 
-}
-
-vector <int> Restantes(tSolucao& s1)
+/****
+ * Restantes(): Criação da quantidade de vértices que ainda faltam serem inseridos no tour
+ * 
+ * 
+ * 
+ * Parâmetros:
+ *                  s1(entrada): vector onde será consultado os valores.
+ *                  quantidade: quantidade de vértices ou posiçẽs
+ * 
+ * Retorno:
+ *                  Um vector com os vértices restantes
+ * 
+ * 
+ ****/
+vector <int> Restantes(tSolucao& s1, int quantidade)
 {
     /*  Criação de todos os vertices    */
     vector <int> v;
-    for(int i = 2; i <= 10; i++)
+    for(int i = 2; i <= quantidade; i++)
     {
         v.push_back(i);
 
     }
-    //2,3,4,5,6,7,8,9,10
 
     /*  Verificação de quais se encontram no primeiro tour, para fazer o CL, que são os que faltam para completar todos os vértices */
     for(int i = 1; i <= 3; i++)
@@ -113,29 +127,26 @@ vector <int> Restantes(tSolucao& s1)
         }
     }
     
-    return v;
-
     
-
+    return v;
 }
 
 /*  &s é uma passagem por referencia, estamos de fato passando seu valor   */
-vector <InsertionInfo>  CalcularCustoInsercao(tSolucao& s, vector <int>& CL)
+vector <InsertionInfo>  CalcularCustoInsercao(tSolucao& s1, vector <int>& CL, int c[][14])
 {
-    int c[1000][1000];
     int i = 0;
     int j;
     /*  Criação da lista de cada k de CL associado a um par de arestas  */
-    vector <InsertionInfo> custoInsercao((s.sequencia.size() - 1) * CL.size());
+    vector <InsertionInfo> custoInsercao((s1.sequencia.size() - 1) * CL.size());
     int l = 0;
     
-    for(int a = 0, b = 1; a < s.sequencia.size() - 1; a++, b++) 
+    for(int a = 0, b = 1; a < s1.sequencia.size() - 1; a++, b++) 
     {
         /*  EX: s' = {1,2,3,4,1}    */
         /*  CL = {5,6,7,8,9}
         /*  i = 1 e j = 2   */
-        i = s.sequencia[a];  /*  a e b são os índices dos vértices  {i,j}    */
-        j = s.sequencia[b];
+        i = s1.sequencia[a];  /*  a e b são os índices dos vértices  {i,j}    */
+        j = s1.sequencia[b];
         /*  K é do tipo inteiro */
         /*  Estamos aqui acessando todos os vértices que faltam adicionar no tour   */
         for (auto k : CL) 
@@ -174,7 +185,7 @@ void InserirNaSolucao(tSolucao& s1, int& selecionado)
 
 int main(void)
 {
-    /*  Iniciamos o primeiro tour com, 1 e 1, e depois vamos vamos colocar 3 vértices aleatórias    */
+    /*  Iniciamos o primeiro tour com, 1 e 1, e depois vamos vamos colocar 3 vértices aleatórios    */
     tSolucao s1 = {{1,1}, 0}; /* Sempre iniciar o primeiro tour com 1 e 1    */
     
     /*  Pegando dados das instancias    */
@@ -183,7 +194,8 @@ int main(void)
     scanf("%s %d", buffer, &dimensao);
     printf("%d\n", dimensao);
 
-    int matriz[dimensao][dimensao];
+    /*  Testando com o bruma14  */
+    int matriz[14][14];
     for(int i = 0; i < dimensao; i++)
     {
         for(int j = 0; j < dimensao; j++)
@@ -192,8 +204,8 @@ int main(void)
         }
     }
 
-    /*  Adicionar depois a partir do 1 não 0    */
-    /*  Verificação se está certo ou não    */
+    
+    /*  Verificação se está certo ou não
     for(int i = 0; i < dimensao; i++)
     {
         for(int j = 0; j < dimensao; j++)
@@ -202,9 +214,11 @@ int main(void)
         }
         printf("\n");
     }
+    */
     
-    
-    Escolher3NumerosAleatorios(s1);    /*  Chamada da função para escolha de três números aleatórios   */
+    /*  Chamada da função para escolha de três números aleatórios   */
+    /*  Enviamos dimensão porque é a quantidade de véritices    */
+    Escolher3NumerosAleatorios(s1, dimensao);   
     /*  Exemplo: s1, agora vira tour para os vertices {1,2,3,4,1}   */
 
     /*
@@ -218,7 +232,7 @@ int main(void)
     */
 
     /*  Precisamos agora criar os vértices restantes que ainda faltam ser inseridos */
-    vector <int> CL = Restantes(s1);     /* Criação do CL */
+    vector <int> CL = Restantes(s1, dimensao);     /* Criação do CL */
     
     /*
     cout << "CL: " << CL.size() << endl;
@@ -228,22 +242,21 @@ int main(void)
     }
     
     
+    cout << CL[0];
     cout << CL.size() << endl;
     */
     
-    vector <InsertionInfo>  custoInsercao;
+    
+    
  
     /*  Enquanto CL não estiver vazio, nós vamos fazer o teste de cada vértice que pertence a CL na entrada de uma aresta {i, j}    */
     /*  Vamos calcular seu custo de inserção e após isso, ordenar todos em ordem crescente  */
     
+    while(!CL.empty())
+    {
+        vector <InsertionInfo>  custoInsercao = CalcularCustoInsercao(s1, CL, matriz);
+    }
     
     
     
-    
-    
-    
-  
-    
-
-
 }
